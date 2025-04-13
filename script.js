227599457898780f4c95f198c9ad17eca97d3d03
ManupaDev <manupadevsuite@gmail.com>
2025-04-13 13:58:36 +0530

    init

---
+++ script.js
@@ -0,0 +1,62 @@
+/*
+console.log("Hello Learners!");
+
+const marks = 89;
+
+if (marks >= 90) {
+  console.log("A");
+} else if (marks >= 80) {
+  console.log("B");
+} else if (marks >= 70) {
+  console.log("C");
+} else if (marks >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
+*/
+
+// for (let i = 1; i <= 10; i=i+1) {
+//   console.log(i);
+// }
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i = i + 1;
+// }
+
+// const add = (a, b) => {
+//   const sum = a + b;
+//   return sum;
+// };
+
+// const sum = add(1, 2);
+// console.log(sum);
+
+// let x = 10;
+
+// const greet = () => {
+//   let x = 20;
+//   console.log("Inside the function", x);
+// };
+
+// greet();
+// console.log("Outside the function", x);
+
+// const userName = "Manupa";
+// const userAge = 25;
+// const userCity = "Kadawatha";
+
+// const user = {
+//   name: "Manupa",
+//   age: 25,
+//   city: "Kadawatha",
+// };
+
+// console.log(user);
+
+// user.age = 26;
+// console.log(user);
+
+const nums = [2, 4, 6, 8, 10];
